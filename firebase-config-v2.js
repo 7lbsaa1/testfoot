793c8fe305b044b62aee89ee76352a837b09eeb8
7lbsaa <7lbsaa1@gmail.com>
2026-05-27 09:45:39 +0300

    Update firebase-config-v2.js

--- firebase-config-v2.js
+++ firebase-config-v2.js
@@ -1,9 +1,7 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// firebase-config.import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// الإعدادات الرسمية المطابقة تماماً لـ Firebase الخاص بمشروعك
 const firebaseConfig = {
     apiKey: "AIzaSyBEZLBEEWH_CLc69RQLemG0UBVU6ym0yIc",
     authDomain: "football-4fed6.firebaseapp.com",
@@ -12,13 +10,11 @@ const firebaseConfig = {
     storageBucket: "football-4fed6.firebasestorage.app",
     messagingSenderId: "605473270039",
     appId: "1:605473270039:web:8d6f74cfc8c9a46279ef52",
-    measurementId: "G-NZF76V3KKS"
+    measurementId: "G-NZF76V3KK5"
 };
 
-// تشغيل الخدمات
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
-// تصدير الأدوات لاستخدامها في صفحات الدخول والتسجيل
 export { auth, database, signInWithEmailAndPassword, createUserWithEmailAndPassword, ref, set };
