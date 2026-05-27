ee165502acf94d4f237aea038a785a3c27449824
7lbsaa <7lbsaa1@gmail.com>
2026-05-27 08:43:36 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,20 +3,22 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// حط البيانات اللي هتنسخها من الصفحة هنا بالظبط:
+// الإعدادات الرسمية المطابقة تماماً لـ Firebase الخاص بمشروعك
 const firebaseConfig = {
-    apiKey: "AIzaSy...", 
+    apiKey: "AIzaSyBEZLBEEWH_CLc69RQLemG0UBVU6ym0yIc",
     authDomain: "football-4fed6.firebaseapp.com",
     databaseURL: "https://football-4fed6-default-rtdb.firebaseio.com",
     projectId: "football-4fed6",
-    storageBucket: "football-4fed6.appspot.com",
+    storageBucket: "football-4fed6.firebasestorage.app",
     messagingSenderId: "605473270039",
-    appId: "1:605473270039:web:8d6f74cfc8c9a46279ef52"
+    appId: "1:605473270039:web:8d6f74cfc8c9a46279ef52",
+    measurementId: "G-NZF76V3KKS"
 };
 
-// تشغيل فايربيز
+// تشغيل الخدمات
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
 
+// تصدير الأدوات لاستخدامها في صفحات الدخول والتسجيل
 export { auth, database, signInWithEmailAndPassword, createUserWithEmailAndPassword, ref, set };
